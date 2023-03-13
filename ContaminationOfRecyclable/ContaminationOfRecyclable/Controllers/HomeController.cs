051a7ceb5d13d5641204930ba67723c848eca553
Tze_Loon_Neoh <tneo0002@student.monash.edu>
2023-03-13 21:08:14 +1100

    Map custom domain to app and bind SSL

--- ContaminationOfRecyclable/ContaminationOfRecyclable/Controllers/HomeController.cs
+++ ContaminationOfRecyclable/ContaminationOfRecyclable/Controllers/HomeController.cs
@@ -15,20 +15,6 @@ namespace ContaminationOfRecyclable.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult GetData()
         {
             Contamination_Models context = new Contamination_Models();
